8ab00b3d9d05aa5a7b4265941e675f6fd758fc61
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 12:18:49 -0400

    backup

--- atm.js
+++ atm.js
@@ -1,3 +1,63 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var buttons = document.getElementsByTagName('input')
+
+// Checking vars:
+
+var inputChecking = document.getElementById('checking-input')
+var depositChecking = buttons[1]
+var withdrawChecking = buttons[2]
+
+
+
+
+
+// // Global variables:
+
+// var buttons = document.getElementsByTagName('input')
+
+// // Checking vars:
+
+// var inputChecking = document.getElementById('checking-input')
+// var depositChecking = buttons[1]
+// var withdrawChecking = buttons[2]
+
+// // Checking input value: 
+// var depositValueChecking = parseInt(inputChecking.value)
+
+
+// // Checking Account Balance:
+// var accBalChecking = document.getElementsByClassName('balance')[0].innerHTML
+//     // split and convert to integer
+// var initialAccValue = parseInt(accBalChecking.split('$')[1])
+
+// /////////
+
+
+
+
+
+// ///// Click listeners:
+// depositChecking.addEventListener('click', function() {
+//   var newBalance = initialAccValue + depositValueChecking
+  
+// })
+
+// withdrawChecking.addEventListener("click", function() {
+//   console.log('helloooo')
+// })
+
+
+
+
+// /////////////////////////////////
+
+// // depositChecking.addEventListener('click', function () {
+
+  
+
+// //   console.log('The current balance is ' + valueInt)
+// //   // Gettin the final balance
+// //   var finalBalance = '$' + (depositValue + valueInt)
+// //   console.log(finalBalance)
+// //   // Add the final balance to HTML
+// //   checkingBalance.innerHTML = finalBalance
+// // })
